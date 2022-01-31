b604b92d0dd25ce2f48574c0c33d3deed1e1d6e4
mortrap <steelall@yandex.ru>
2022-01-31 05:56:23 +0300

    get html from warc

--- mysu.py
+++ mysu.py
@@ -2,15 +2,15 @@ import pprint
 pp = pprint.PrettyPrinter(indent=0)
 # Needs urllib3 2, https://github.com/urllib3/urllib3#installing
 import urllib3
-import requests
+
 
 
 
 
 #import extruct
 from fastwarc.stream_io import *
-from fastwarc.warc import ArchiveIterator, WarcRecordType, is_http
-
+from fastwarc.warc import ArchiveIterator, WarcRecordType, WarcRecord, is_http
+from fastwarc.stream_io import BufferedReader
 # fastwarc: test the streaming parsing
 
 url ='https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2021-10/segments/1614178347293.1/warc/CC-MAIN-20210224165708-20210224195708-00004.warc.gz'
@@ -19,30 +19,67 @@ url ='https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2021-10/segments/1
 req = urllib3.request("GET", url, preload_content=False)
 #print(req.content)
 #head = WarcHeaderMap()
-for record in ArchiveIterator(req, record_types=WarcRecordType.response, func_filter=is_http, max_content_length = 512000):
-     assert record.is_http
-     pp.pprint(record.content_length )
-     pp.pprint(record.http_content_type)
-#Getting html from record
+
+def read_cl(record , cl):
+     return record.reader.read(cl)
+is_eof = False
+records = []
+with open('test', 'w') as file:
+
+     for record in ArchiveIterator(req, record_types=WarcRecordType.response, func_filter=lambda r: r.headers.get('WARC-Identified-Payload-Type') == 'text/html', max_content_length = 512000):
+          print('Payload digest OK:', record.verify_payload_digest())
+          if record.content_length>0:
+              file.write(record.reader.read(record.content_length).decode('utf-8'))
+          pp.pprint(record.content_length)
+          file.close()
+          break
+     #if str(record.http_headers.status_code) in record.http_headers.status_line:
+     #if record.http_headers.get('Content-Type').startswith('text/html'): 
+     
+               
+               
+               
+      
+#get html from record
+
+
+
+    
+          # try:
+          #   lines = f.readlines()
+          #   idx_response_ok = None
+          #   for idx, line in enumerate(lines):
+          #      if line.startswith('HTTP'):
+          #           if '200' in line:
+          #               idx_response_ok = idx
+          #           else:
+          #               idx_response_ok = -1
+          #           break
+          #   if idx_response_ok is None:
+          #           logging.info('Abort %s: no HTTP status code found' % warcfile)
+          #           return None
+          #   elif idx_response_ok == -1:
+          #           logging.info('Abort %s: HTTP status code not 200' % warcfile)
+          #           return None
+
+          #   idx_html = None
+          #   for idx, line in enumerate(lines[idx_response_ok + 1:]):
+          #       if line.startswith('<!DOCTYPE html>'):
+          #           idx_html = idx
+          #           break
+
+          #   return '\n'.join(lines[idx_html:])
+          # except Exception as e:
+          #   logging.error('Abort %s: error when reading file; %s' % (warcfile, str(e)))
+          #   return None
+      # get html   
+        
+
 
 
     
      
     #html = '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">\n<HTML><HEAD>\n<TITLE>404 Not Found</TITLE>\n</HEAD><BODY>\n<H1>Not Found</H1>\
-
-
-    # --- Snek #5 -------
-
-#     def get_html(record):
-#     #print('<',record['header']['type'], '>')
-#     if record['header']['type'] == WarcRecordType.warc:
-#         return record['body'].decode()
-
-
-
-
-# from e import e
-# from exhtml import html
 
 #     stream = GZipStream(FileStream('warcfile.warc.gz', 'rb'))
 
@@ -109,6 +146,3 @@ for record in ArchiveIterator(req, record_types=WarcRecordType.response, func_fi
 #     df = df.append(row, ignore_index=True)
 
 # df.head(10).sort_values(by='microdata', ascending=False)
-
-
-
