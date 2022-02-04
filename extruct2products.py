251e98a6c14bae42ceb24c51db8d839edf110cf5
mortrap <steelall@yandex.ru>
2022-02-04 11:44:02 +0300

    added rdfa-filter, testing, noready

--- extruct2products.py
+++ extruct2products.py
@@ -1,73 +1,121 @@
 import extruct
-from llog import log
+#from llog import log
 import datetime
 import yaml
-import json
+#import json
 
-
+# не прошла на другом файле из примера, причину искал print statement
 def extruct2products(html):
   products = []
   dick = extruct.extract(html, uniform=True, errors='ignore')
-
+  ## Hier ok
+  #microdata
   if 'microdata' in dick:
     for item in dick['microdata']:
-      if item['@type'] != 'Product':
+      ## Hier not
+      #print(item)
+      if item['@type'] != 'Product' and item['@type'] != 'Offer' and item['@type'] != 'Brand': # or ?
         continue
-      if not 'name' in item:
+      #ok
+      #print(item)
+      ## Hier not
+      product ={}
+      if not 'name' in item and not 'price' in item:# and not 'Brand' in item: #or ?
         continue
-      if not 'offers' in item:
-        continue
-      product = {}
-      for key in ['name', 'brand', 'description', 'gtin8', 'image']:
+      #ok
+      #print(item)
+      for key in ['name']:
         if key in item:
           product[key] = item[key]
-
+      # Протащить имя из Brand
+      if not 'offers' in item and 'name' in item:
+        continue
+      #ok
+      #print(item)
+      
+      for key in ['name', 'brand', 'description', 'gtin8', 'image', 'sku', 'url']:
+        if key in item:
+          product[key] = item[key]
+      
+      #print(product)
+      #print(yaml.dump(dick))
       if item['offers']:
         offers = item['offers']
+      
         in_stock = 'availability' in offers and offers['availability'] == 'http://schema.org/InStock'
         new = 'itemCondition' in offers and offers['itemCondition'] == 'http://schema.org/NewCondition'
         priceCurrency = offers['priceCurrency'] if offers['priceCurrency'] else ''
         price = offers['price'] if offers['price'] else ''
-        if price and priceCurrency and in_stock and new:
+        #print(price, priceCurrency)#ok 1190.00 RUB #and
+        if price and priceCurrency or in_stock and new:
           product['price'] = price
           product['currency'] = priceCurrency
           products.append(product)
 
+   #opengraph       
   if 'opengraph' in dick:
     for item in dick['opengraph']:
       if item['@type'] != 'product':
         continue
-      if not 'og:title' in item:
+      
+      if not 'og:title' in item  and not 'og:url' in item:
         continue
-      if not 'product:price:amount' in item:
+      
+      if not 'product:price:currency' and not 'product:price:amount':
         continue
-      if not 'product:price:currency' in item:
-        continue
+      #print(item)
+      
       product = {}
       for [key, kʹ] in [['og:title', 'name'], ['og:description', 'description'], ['og:image', 'image'],
                         ['product:price:currency', 'currency'], ['product:price:amount', 'price']]:
         if key in item:
           product[kʹ] = item[key]
-
+        #print(products) 
+      #print(product['name']) Ok
+# @value?
+#rdfa
+  if 'rdfa' in dick:
+    for item in dick['rdfa']:
+      if not 'http://ogp.me/ns#title' in item: continue
+      
+      if not 'http://ogp.me/ns#type' in item: continue
+      
+      if not 'product:price:amount' in item: continue
+      print(item)
+      if not 'product:price:currency' in item: continue 
+      
+      if not '@value' in item: continue
+      product = {}
+      for [key, kʹ] in [['http://ogp.me/ns#title', 'name'], ['http://ogp.me/ns#description', 'description'], ['http://ogp.me/ns#image:', 'image'],
+                        ['product:price:currency', 'currency'], ['product:price:amount', 'price']]:
+        if key in item:
+          product[kʹ] = item[key]
+  # if 'dublincore' in dick:
+  #   for item in dick['duplincore']:
+  #     print(item)
       # ⌥ consider merging different formats into a single results,
       #    or removing product duplicates by name
+      
       if product['name'] in [i['name'] for i in products]:
         continue
-
+      
       products.append(product)
 
   return products
+  
 
-
+# периодически возвращаясь к файлу, под который это было написано изначально
 def test_extruct2products():
-  html = open('test-data/schema-org.html').read()
+  html = open('test-data/Наушники JBL T210, чёрный купить в Красноярске _ Технопарк.html', 'r').read()
   start = datetime.datetime.now()
   got = extruct2products(html)
   delta = datetime.datetime.now() - start
   #log(delta)
-  log(yaml.dump(got))
-  log(json.dumps(got, indent=2))
+  print(yaml.dump(got))
+  #print(json.dumps(got, indent=2))
 
 
 if __name__ == "__main__":
   test_extruct2products()
+# @value python with output from "rdfa
+
--- mysu.py
+++ mysu.py
@@ -1,12 +1,14 @@
+
 import pprint
 pp = pprint.PrettyPrinter(indent=0, width=50)
 # Needs urllib3 2, https://github.com/urllib3/urllib3#installing
 import urllib3
 import extruct
 import numpy as np
-from lxml import etree
-import codecs
-
+from extruct2products import extruct2products as ex
+import requests
+import urllib3
+import yaml
 #import extruct
 #from fastwarc.stream_io import *
 from fastwarc.warc import ArchiveIterator, WarcRecordType, WarcRecord, is_http
@@ -16,9 +18,16 @@ from fastwarc.warc import ArchiveIterator, WarcRecordType, WarcRecord, is_http
 url ='https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2021-10/segments/1614178347293.1/warc/CC-MAIN-20210224165708-20210224195708-00004.warc.gz'
 #req=requests.get(url, stream = True)
 # use urllib3 to open an URL stream
-req = urllib3.request("GET", url, preload_content=False)
+
+#req =  requests.request(method="get",url=url) # its not working
+
+
+req = urllib3.request("GET", url, preload_content=False)# its working
+
+    
+        
 #print(req.content)
-#head = WarcHeaderMap()
+
 def exne (html):
     metadata = extruct.extract(html , uniform=True, errors='ignore')     
     return metadata
@@ -54,19 +63,19 @@ for record in ArchiveIterator(req, record_types = WarcRecordType.response, func_
     #mb forgot lxml.etree
     if record.content_length>0:
         # !Записываем в словарь что считали при помощи extruct
-        #tree = etree.parse((record.reader.read(record.content_length)))
-        #metadata = exne(tree)
+        
         reccl = record.content_length
         metadata = exne((record.reader.read(reccl)).decode('utf-8', 'ignore'))
+        
         # !Если тип метаданных json-ld or microdata извлекаем, 
-        if uses_metadata_type(metadata, 'json-ld') or uses_metadata_type(metadata, 'microdata'):
-            # Не понимаю как отсеять нужные мне значения, работа со словарем
-                if'@type' in str(metadata.values()):
-                        array = np.array(list(metadata.values()))
-                        array2 = np.array_split(array, 3)
-                        if np.isin ('@type', array2, 0, -1) & np.isin ('Product', array2, 0,-1) & np.isin ('offers', array2, 0, -1):
-                                                    ixgrid = np.ix_([3, -1])
-                                                    print(array[ixgrid])
+        # if uses_metadata_type(metadata, 'json-ld') or uses_metadata_type(metadata, 'microdata'):
+        #     # Не понимаю как отсеять нужные мне значения, работа со словарем
+        #         if'@type' in str(metadata.values()):
+        #                 array = np.array(list(metadata.values()))
+        #                 array2 = np.array_split(array, 3)
+        #                 if np.isin ('@type', array2, 0, -1) & np.isin ('Product', array2, 0,-1) & np.isin ('offers', array2, 0, -1):
+        #                                             ixgrid = np.ix_([3, -1])
+        #                                             print(array[ixgrid])
 
                 #metadata.clear()
                 #product = get_dictionary_by_key_value(metadata, "microdata", "Product")
@@ -83,55 +92,8 @@ for record in ArchiveIterator(req, record_types = WarcRecordType.response, func_
                     # npArray = np.array(data)
                     # arr = np.isin('@type', npArray)
                     # print(arr)
-                    
-                    
-    #else:
-        #continue
-            # count+=1
-            # if count ==3:
-            #     break
-     #if str(record.http_headers.status_code) in record.http_headers.status_line: 
-
-#     stream = GZipStream(FileStream('warcfile.warc.gz', 'rb'))
-
-# for record in ArchiveIterator(stream, record_types=WarcRecordType.request | WarcRecordType.response):
-#     pass
-# from fastwarc.stream_io import *
-
-# # GZip:
-# stream = GZipStream(open('warcfile.warc.gz', 'rb'))
-
-# # LZ4:
-# stream = LZ4Stream(open('warcfile.warc.lz4', 'rb'))
-# string html 
-# def extract_metadata(text):
-
-
-
-
-# if key_exists(metadata['microdata'], 'Product') or key_exists(metadata['json-ld'], 'Product') or key_exists(metadata['opengraph'], 'Product'):
-#     pp.pprint (metadata)
-# uses_metadata_type(metadata, 'json-ld')
-# uses_metadata_type(metadata, 'opengraph')
-# uses_metadata_type(metadata, 'microdata')    
-# uses_metadata_type(metadata, 'rdfa')
-# key_exists(metadata['microdata'], 'Product')
-# key_exists(metadata['opengraph'], 'Product')
-# key_exists(metadata['json-ld'], 'Product')
-
-# df = pd.DataFrame(columns = ['url', 'microdata', 'json-ld', 'opengraph'])
-
-# for url in sites:    
-#     metadata = extract_metadata(url)
-#     urldata = urlparse(url)
-
-#     row = {
-#         'url': urldata.netloc, 
-#         'microdata': uses_metadata_type(metadata, 'microdata'),
-#         'json-ld': uses_metadata_type(metadata, 'json-ld'),
-#         'opengraph': uses_metadata_type(metadata, 'opengraph'),               
-#     }
-
-#     df = df.append(row, ignore_index=True)
-
-# df.head(10).sort_values(by='microdata', ascending=False)
+#     import http.client
+# c = http.client.HTTPSConnection('shops.mercatos.net')
+# c.request("GET", "/ru/")
+# r = c.getresponse()
+# print(r.status)                
